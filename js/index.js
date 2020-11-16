d9cb089046441c8b4fea8c2bd72bd6a670efdbda
Leonid Korsakov <KorsakovLA@mail.ru>
2020-11-16 11:11:15 +0300

    Теперь можно добавлять новые задачи

--- js/index.js
+++ js/index.js
@@ -1,23 +1,38 @@
 const newTasksElem = document.querySelector('.new-tasks');
+const newTasksField = newTasksElem.querySelector('.tasks-field')
 const continueTasksElem = document.querySelector('.continue-tasks');
 const finishedTasksElem = document.querySelector('.finished-tasks');
-const modal = document.querySelector('.modal');
+const modalTask = document.querySelector('.modal-task');
 const openModalButton = document.querySelector('.open-modal-button');
+const addTaskText = document.querySelector('.add-task-text');
 const closeButton = document.querySelector('.close-button');
+const addTaskButton = document.querySelector('.add-task-button');
+
+const newTasksArr = [];
+const continueTasksArr = [];
+const finishedTasksarr = [];
 
 
 
+function createNewTask(taskText) {
+	const taskForm = document.createElement('div');
+	taskForm.classList.add('task');
+	taskForm.innerHTML = `<p>${taskText}</p>`;
+	newTasksField.appendChild(taskForm);
+}
 
-
-
-
+addTaskButton.addEventListener('click', event => {
+	createNewTask(addTaskText.value);
+	addTaskText.value = "";
+	modalTask.classList.toggle('unvisible');
+});
 
 openModalButton.addEventListener('click', () => {
-	modal.classList.toggle('unvisible');
+	modalTask.classList.toggle('unvisible');
 });
 
 closeButton.addEventListener('click', () => {
-	modal.classList.toggle('unvisible');
+	modalTask.classList.toggle('unvisible');
 });
 
 
