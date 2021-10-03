5304bc5692ad98280f32cc9f2c503f4212152d04
Leonid Korsakov <KorsakovLA@mail.ru>
2021-10-03 23:09:48 +0300

    подвинута кнопка submit

--- develop/js/modules/pages/settings.js
+++ develop/js/modules/pages/settings.js
@@ -1,7 +1,8 @@
 export function settings() {
 	return `
-		<div class="settings-container">
+	<div class="settings-container">
 
+	<div class="main-settings">
 		<h3>Main settings</h3>
 		<div class="setting-option main-theme">
 			<header class="setting-option_header">
@@ -9,14 +10,18 @@ export function settings() {
 			</header>
 			<div class="switch-btn"></div>
 		</div>
+	</div>
 
+	<div class="special-options">
 		<h3>Spicial options</h3>
 		<div class="setting-option">
 			Blind mode
 			<div class="switch-btn"></div>
 		</div>
-
-		<button class="submit-button">Submit</button>
 	</div>
+
+	<button class="submit-btn">Submit</button>
+	
+</div>
 	`
 }
