098f5503b2a3451733a3fb3d0881dc2aad2056ff
Leonid Korsakov <KorsakovLA@mail.ru>
2021-10-03 18:46:40 +0300

    рассмотрен вариант перехода на main через logo

--- develop/js/index.js
+++ develop/js/index.js
@@ -1,4 +1,12 @@
 import '../css/index.css';
 import { showTasksFrames } from "./modules/showTasksFrames/showTasksFrames.js";
 
+const logo = document.querySelector('.logo');
+const main = document.querySelector('.main');
+
+logo.addEventListener('click', () => {
+	main.innerHTML = '';
+	showTasksFrames();
+})
+
 showTasksFrames();
