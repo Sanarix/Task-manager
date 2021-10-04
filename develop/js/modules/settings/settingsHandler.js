669d8f8dbf55939a198b14ae02a477539bacdbc1
Leonid Korsakov <KorsakovLA@mail.ru>
2021-10-04 19:57:10 +0300

    начало реализации применения настроек

--- develop/js/modules/settings/settingsHandler.js
+++ develop/js/modules/settings/settingsHandler.js
@@ -1,9 +1,15 @@
+import { modeTheme } from "./toggleTheme";
+
 export function settingsHandler() {
 	const switchs = document.querySelectorAll('.switch-btn');
 	
 	for (let switchBtn of switchs) {
 		switchBtn.addEventListener('click', () => {
 		switchBtn.classList.toggle('switch-on');
+
+		if(switchBtn.dataset.mode == 'theme') {
+			modeTheme();
+		}
 		})
 	}
 }
---
+++ develop/js/modules/settings/toggleTheme.js
@@ -0,0 +1,3 @@
+export function modeTheme() {
+	document.body.classList.toggle('dark-theme');
+}
