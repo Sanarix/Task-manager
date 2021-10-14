a9e1a7f0132d4a28dc36323118c1289a950ceda5
Leonid Korsakov <KorsakovLA@mail.ru>
2021-10-14 10:28:53 +0300

    починил свитчи в настройках

--- develop/js/modules/settings/updateSettings.js
+++ develop/js/modules/settings/updateSettings.js
@@ -1,7 +1,3 @@
-import { settings } from "../pages/settings";
-
 export function updateSettings() {
-	const update = document.querySelector('.main').innerHTML;
-	settings(update);
-	console.log('updateSettings отработал, передаю значение в settings');
+	console.log('update settings');
 }
