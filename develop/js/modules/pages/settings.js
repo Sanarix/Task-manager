5db01f6dc23dfce839f961f981d95c5a2b16dea2
Leonid Korsakov <KorsakovLA@mail.ru>
2021-10-26 11:51:24 +0300

    удалил пустой файл, переделал settings

--- develop/js/modules/pages/settings.js
+++ develop/js/modules/pages/settings.js
@@ -1,31 +1,30 @@
-const defaultSettings = `<div class="settings-container">
-<div class="main-settings">
-	<h3>Main settings</h3>
-
-	<div class="setting-option main-theme">
-		<header class="setting-option_header">
-			Dark mode
-		</header>
-		<div class="switch-btn" data-mode="theme"></div>
-	</div>
-
-</div>
-
-<div class="special-options">
-	<h3>Spicial options</h3>
-
-	<div class="setting-option">
-		Blind mode
-		<div class="switch-btn" data-mode="blind"></div>
-	</div>
-
-</div>
-
-<button class="submit-btn">Submit</button>
-
-</div>
-`;
-
 export function settings() {
-	return defaultSettings;
+	return `
+		<div class="settings-container">
+		<div class="main-settings">
+			<h3>Main settings</h3>
+
+			<div class="setting-option main-theme">
+				<header class="setting-option_header">
+					Dark mode
+				</header>
+				<div class="switch-btn" data-mode="theme"></div>
+			</div>
+
+		</div>
+
+		<div class="special-options">
+			<h3>Spicial options</h3>
+
+			<div class="setting-option">
+				Blind mode
+				<div class="switch-btn" data-mode="blind"></div>
+			</div>
+
+		</div>
+
+		<button class="submit-btn">Submit</button>
+
+	</div>
+	`
 }
