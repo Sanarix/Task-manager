474c7e133f2124a50f9e82c650800774e546c330
Leonid Korsakov <KorsakovLA@mail.ru>
2021-11-17 19:33:33 +0300

    исправлена логика стилей для new-task-frame

--- develop/js/functions/task/renderTasksInFrame.js
+++ develop/js/functions/task/renderTasksInFrame.js
@@ -1,17 +1,20 @@
 export function renderTasksInFrame(element, selector) {	
 
-	if(!element || !selector) {
-		throw new Error('renderTasksInFrame must contain 2 args');
-	}
-	
-	const selectorItem = document.querySelector(selector);
-	const selectorItemChildrens = selectorItem.children;
-
-	for( let child of selectorItemChildrens) {
-		if(child.classList.contains('task-frame_img')){
-			child.remove();
+		if(!element || !selector) {
+			throw new Error('renderTasksInFrame must contain 2 args');
 		}
-	}
-	
-	selectorItem.prepend(element);
+		
+		const selectorItem = document.querySelector(selector);
+		const selectorItemChildrens = selectorItem.children;
+
+		for( let child of selectorItemChildrens) {
+			if(child.classList.contains('task-frame_img')){
+				child.remove();
+				selectorItem.style.justifyContent = 'flex-start';
+			}
+		}
+
+		
+		
+		selectorItem.prepend(element);
 }
