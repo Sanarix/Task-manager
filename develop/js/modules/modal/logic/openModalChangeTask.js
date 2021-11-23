9142bdc43ca63aad872a177d72753e4f602189e7
Leonid Korsakov <KorsakovLA@mail.ru>
2021-11-23 12:54:53 +0300

    добавлена возможность редактировать задачи

--- develop/js/modules/modal/logic/openModalChangeTask.js
+++ develop/js/modules/modal/logic/openModalChangeTask.js
@@ -1,22 +1,25 @@
 import { openModal } from "./openModal";
 import { newTask } from "../newTask";
 import { closeModal } from "./closeModal";
-import { set, ref, update, remove } from "firebase/database";
+import { set, ref, remove } from "firebase/database";
 
 export function openModalChangeTask(element, db) {
 	const elementID = element.dataset.id;
 	const cardHeader = element.querySelector('.task-card_header').textContent.trim();
 	const cardText = element.querySelector('.task-card_text').textContent.trim();
 
-	function updateTask(button) {
-		button.addEventListener('click', event => {
-			event.preventDefault();
-	})
+	function updateTask() {
+			const task = document.querySelector('.newTask');
+			const header = task.querySelector('.title').textContent.trim();
+			const text = task.querySelector('.task-text').textContent.trim();
+			set(ref(db, `tasks/${elementID}/`), {
+				"taskHeader": header,
+				"taskText": text
+			})
+			closeModal(true);
 }
 
-	function deleteTask(button) {
-		button.addEventListener('click', event => {
-			event.preventDefault();
+	function deleteTask() {
 			const tasks = document.querySelectorAll('.task-card');
 			for (const task of tasks) {
 				if(task.dataset.id === elementID) {
@@ -25,13 +28,12 @@ export function openModalChangeTask(element, db) {
 			}
 			remove(ref(db, `tasks/${elementID}`));
 			closeModal(true);
-		})
 	}
 
 	openModal(null, newTask(cardHeader ,cardText));
 
 	const buttonOk = document.querySelector('.btn-ok');
 	const buttonNok = document.querySelector('.btn-nok');
-	updateTask(buttonOk);
-	deleteTask(buttonNok);
+	buttonOk.addEventListener('click', updateTask);
+	buttonNok.addEventListener('click', deleteTask);
 }
