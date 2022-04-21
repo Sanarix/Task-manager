b4383071d85e44bbf3d86f4b5fa681694d010ece
Leonid Korsakov <KorsakovLA@mail.ru>
2022-04-21 19:36:25 +0300

    промежуточные результаты функции moveTask

--- develop/js/modules/modal/logic/moveTask.js
+++ develop/js/modules/modal/logic/moveTask.js
@@ -5,26 +5,55 @@ export function moveTask(element, db, uid) {
 	const taskCardId = element.dataset.id;
 	const main = document.querySelector('main');
 
-	function readyToMove() {
-		const movingTaskCard = taskCard;
-		console.log(movingTaskCard);
-	}
-	
 	taskCard.addEventListener('mousedown', event => {
-		event.stopPropagation();
 		event.preventDefault();
-		console.log('Начато перемещение');
+		event.stopPropagation();
+		taskCard.classList.add('moving');
+		console.dir(taskCard);
+		console.log(taskCard.parentElement.offsetWidth);
 
-		// Далее логика перемещения:
-		// копируем задачу, вставляем в body, удаляем задачу из new-task-frame 
-		// делаем так чтобы задача перемещалась за мышкой
-		// при наведении на другой фрейм и отпускании мыши, добавляем задачу в тот фрейм на которым находится мышь.
+		moving(taskCard);
 	})
 
-	main.addEventListener('mouseup', event => {
-		event.stopPropagation();
-		event.preventDefault();
-		console.log('Перемещение закончено');
-	})
+	function moving(movingElement) {
+		function move(event) {
+			movingElement.style.top = `${event.clientX}px`;
+			movingElement.style.left = `${event.clientX}px`;
+		}
+		document.addEventListener('mousemove', (event) => move(event));
+		endingMove(movingElement, move);
+	}
+
+	function endingMove(movingElement, move) {
+		document.addEventListener('mouseup', event=> {
+			event.preventDefault();
+			event.stopPropagation();
+			movingElement.classList.remove('moving');
+			document.removeEventListener('mousemove', (event) => move(event))
+			console.log('Зафиксировали элемент в другом фрейме');
+		})
+	}
+
+	// function readyToMove() {
+	// 	const movingTaskCard = taskCard;
+	// 	console.log(movingTaskCard);
+	// }
+	
+	// taskCard.addEventListener('mousedown', event => {
+	// 	event.stopPropagation();
+	// 	event.preventDefault();
+	// 	console.log('Начато перемещение');
+
+	// 	// Далее логика перемещения:
+	// 	// копируем задачу, вставляем в body, удаляем задачу из new-task-frame 
+	// 	// делаем так чтобы задача перемещалась за мышкой
+	// 	// при наведении на другой фрейм и отпускании мыши, добавляем задачу в тот фрейм на которым находится мышь.
+	// })
+
+	// main.addEventListener('mouseup', event => {
+	// 	event.stopPropagation();
+	// 	event.preventDefault();
+	// 	console.log('Перемещение закончено');
+	// })
 
 }
