7b51aef6bf5c2fd66f970d8ada98681d4b95148f
Leonid Korsakov <KorsakovLA@mail.ru>
2022-06-09 11:56:29 +0300

    исправлено отображение прелоадера в new-task-frame

--- develop/js/modules/loader/addLoader.js
+++ develop/js/modules/loader/addLoader.js
@@ -2,5 +2,6 @@ import loader from "./loader.js";
 
 export function addLoader(selector) {
 	const element = document.querySelector(`${selector}`);
+	element.style.justifyContent = 'flex-start';
 	element.innerHTML = loader();
 }
