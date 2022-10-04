1cf2970174d8d6cf53bb7684fb3454ffa159a483
Leonid Korsakov <KorsakovLA@mail.ru>
2022-10-04 20:13:59 +0300

    добавлен лоадер для task-frame

--- develop/js/modules/loader/loader.js
+++ develop/js/modules/loader/loader.js
@@ -1,5 +1,7 @@
 export default function loader() {
 	return `
-		<div class="loader">Loading...</div>
+	<div class="loader"><div></div><div></div><div></div><div></div></div>
 	`
 }
+
+{/* <div class="loader">Loading...</div> */}
