d3f5450ccb968ab48210896407ed6b412ecd59a1
Leonid Korsakov <KorsakovLA@mail.ru>
2022-11-11 00:15:33 +0300

    Добавлена безопасность

--- develop/js/index.js
+++ develop/js/index.js
@@ -1,4 +1,5 @@
 import '../css/index.css';
+import authConfig from '../../authConfig.json'
 import { renderPages } from './modules/pages/renderPages.js';
 import { buttonLogInListener } from './modules/modal/logic/buttonLogInListener.js';
 import { getAuth} from '@firebase/auth';
@@ -7,13 +8,13 @@ import { realtimeDatabase } from './modules/realtimeDatabase.js';
 import { isAuth } from './functions/auth/isAuth';
 
 const initApp = initializeApp({
-	apiKey: "AIzaSyAuzFKnVdCA3v64P0Ea6FuV6aJDVKC-S6E",
+	apiKey: `${authConfig.apiKey}`,
 	authDomain: "task-manager-5c313.firebaseapp.com",
 	databaseURL: "https://task-manager-5c313-default-rtdb.firebaseio.com",
 	projectId: "task-manager-5c313",
 	storageBucket: "task-manager-5c313.appspot.com",
-	messagingSenderId: "453985294089",
-	appId: "1:453985294089:web:5c97598f8abe0e0bb7e5d8"
+	messagingSenderId: `${authConfig.messagingSenderId}`,
+	appId: `${authConfig.appId}`
 })
 
 const auth = getAuth();
