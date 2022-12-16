52df47d611a73bcc91d34f8309ccda97581b19e8
Leonid Korsakov <KorsakovLA@mail.ru>
2022-12-16 20:38:24 +0300

    конфиг webpack на стандарт es

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,61 @@
-const path = require('path')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const copyPlugin = require('copy-webpack-plugin')
+import { resolve } from 'path'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import HTMLWebpackPlugin from 'html-webpack-plugin'
+import copyPlugin from 'copy-webpack-plugin'
 
-module.exports = {
-  entry: './develop/js/index.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-		filename: '[name].js',
-  },
-  devServer: {
-    port: 8888
-  },
-  cache: {
-    type: 'memory'
-  },
-  devtool: 'source-map',
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: './develop/index.html',
-      filename: 'index.html'
-    }),
-    new HTMLWebpackPlugin({
-      template: './develop/pages/about.html',
-      filename: 'about.html'
-    }),
-    new HTMLWebpackPlugin({
-      template: './develop/pages/contacts.html',
-      filename: 'contacts.html'
-    }),
-    new CleanWebpackPlugin(),
-    new copyPlugin({
-      patterns: [
-        {from: './develop/img', to: 'img'}
-      ]
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.s[ca]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.(png|jpe?g|svg)$/i,
-        loader: 'file-loader',
-        options: {
-          name: 'img/[name].[ext]'
-        }
-      }
-    ]
+export const entry = './develop/js/index.js'
+export const output = {
+  path: resolve(__dirname, 'public'),
+  filename: '[name].js',
+}
+export const devServer = {
+  port: 8888
+}
+export const cache = {
+  type: 'memory'
+}
+export const devtool = 'source-map'
+export const optimization = {
+  splitChunks: {
+    chunks: 'all'
   }
+}
+export const plugins = [
+  new HTMLWebpackPlugin({
+    template: './develop/index.html',
+    filename: 'index.html'
+  }),
+  new HTMLWebpackPlugin({
+    template: './develop/pages/about.html',
+    filename: 'about.html'
+  }),
+  new HTMLWebpackPlugin({
+    template: './develop/pages/contacts.html',
+    filename: 'contacts.html'
+  }),
+  new CleanWebpackPlugin(),
+  new copyPlugin({
+    patterns: [
+      { from: './develop/img', to: 'img' }
+    ]
+  })
+]
+export const module = {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.s[ca]ss$/i,
+      use: ['style-loader', 'css-loader', 'sass-loader']
+    },
+    {
+      test: /\.(png|jpe?g|svg)$/i,
+      loader: 'file-loader',
+      options: {
+        name: 'img/[name].[ext]'
+      }
+    }
+  ]
 }
