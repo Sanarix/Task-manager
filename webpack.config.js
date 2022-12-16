061f2bd3a78d9479c956c87943514d91b24b2f1b
Leonid Korsakov <KorsakovLA@mail.ru>
2022-12-16 23:06:19 +0300

    еще одна попытка решить проблему cleanwebpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const copyPlugin = require('copy-webpack-plugin')
 
