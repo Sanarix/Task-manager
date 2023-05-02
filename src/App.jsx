ecfd40ad4dd1bf20642b6c66dc6f8df472bda264
Leonid Korsakov <KorsakovLA@mail.ru>
2023-05-02 14:35:17 +0300

    Привёл код в порядок, перенёс useEffectы в useTasks

--- src/App.jsx
+++ src/App.jsx
@@ -8,52 +8,19 @@ import initApp from './functions/initApp';
 import useAuth from './hooks/useAuth';
 import useTasks from './hooks/useTasks';
 import useUser from './hooks/useUser';
-import useDb from './hooks/useDb';
-
 
 import { useState, useEffect } from 'react';
 
-//Для useEffect
-import getTasks from './modules/taskController/getTasks';
-import realtimeDatabase from './modules/database/realtimeDatabase';
-
 export default function App() {
-const {
-  newTasks,
-  setNewTasks,
-  progressTasks,
-  setProgressTasks,
-  finishedTasks,
-  setFinishedTasks
-} = useTasks();
-
 const {user, setUser} = useUser();
 const app = initApp();
 const auth = useAuth( setUser );
-const {db, setDatabase} = useDb();
 
-useEffect(() => {
-  if(auth) {
-    setDatabase(realtimeDatabase(app));
-  }else {
-    setNewTasks([]);
-    setProgressTasks([]);
-    setFinishedTasks([]);
-  }
-}, [auth])
-
-useEffect(() => {
-  if(db) {
-    getTasks(db, user.uid, setNewTasks, setProgressTasks, setFinishedTasks);
-  }
-}, [db])
-
-//exclude duplicates of tasks
-useEffect(() => {
-  setNewTasks(newTasks)
-  setProgressTasks(progressTasks)
-  setFinishedTasks(finishedTasks)
-}, [newTasks, progressTasks, finishedTasks, auth])
+const {
+  newTasks,
+  progressTasks,
+  finishedTasks,
+} = useTasks(auth, app, user);
 
   return (
     <>
--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -1,9 +1,37 @@
+import { useEffect } from "react";
 import { useState} from "react";
+import realtimeDatabase from "../modules/database/realtimeDatabase";
+import useDb from "./useDb";
+import getTasks from "../modules/taskController/getTasks";
 
-export default function useTasks() {
+export default function useTasks(auth, app, user) {
 	const [newTasks, setNewTasks] = useState([]);
 	const [progressTasks, setProgressTasks] = useState([]);
 	const [finishedTasks, setFinishedTasks] = useState([]);
+	const {db, setDatabase} = useDb();
+
+	useEffect(() => {
+		if(auth) {
+			setDatabase(realtimeDatabase(app));
+		}else {
+			setNewTasks([]);
+			setProgressTasks([]);
+			setFinishedTasks([]);
+		}
+	}, [auth])
+
+	useEffect(() => {
+		if(db) {
+			getTasks(db, user.uid, setNewTasks, setProgressTasks, setFinishedTasks);
+		}
+	}, [db])
+
+	//exclude duplicates of tasks
+useEffect(() => {
+  setNewTasks(newTasks)
+  setProgressTasks(progressTasks)
+  setFinishedTasks(finishedTasks)
+}, [newTasks, progressTasks, finishedTasks, auth])
 
 	return {
 		newTasks,
